4be249e8d5abc4ea570fb85a8c84daed1ae025d2
DeviSerene <deviserene@gmail.com>
2018-10-09 12:30:10 +0100

    Shader Program

--- src/myengine/ShaderProgram.h
+++ src/myengine/ShaderProgram.h
@@ -1,6 +1,23 @@
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include <fstream>
+#include <iostream>
+#include <string>
 
+class VertexArray;
 
 class ShaderProgram
 {
+public:
+	ShaderProgram(std::string vert, std::string frag);
+	void Draw(VertexArray& vertexArray);
+	void SetUniform(std::string uniform, glm::vec4 value);
+	void SetUniform(std::string uniform, float value);
+	void SetUniform(std::string uniform, glm::mat4 value);
 
+	GLuint GetId() { return m_id; }
+
+private:
+	GLuint m_id;
 };
--- src/myengine/VertexArray.cpp
+++ src/myengine/VertexArray.cpp
@@ -46,7 +46,7 @@ GLuint VertexArray::GetId()
 	{
 		glBindVertexArray(m_id);
 
-		for (int i = 0; i < 2; i++)
+		for (int i = 0; i < 2; i++) //...there must be a better way to do this.
 		{
 			if (m_buffers[i]) //was something defined here?
 			{
